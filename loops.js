612a139e6eaa7aa73c3257b3a6a0c63516396322
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-05 01:38:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop(array){
   for (i = 0; i < 25; i++) {
     if(i===1){
       array.push("I am 1 strange loop.");
-    }else(i==1){
+    }{
       array.push("I am ${i} strange loops.")
     }
   }
