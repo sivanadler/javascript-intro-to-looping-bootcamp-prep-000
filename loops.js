4470effafb541d3acc55ca2fd9982d965cb2113f
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-05 01:39:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,14 +1,14 @@
 var array = [array, t]
  
 function forLoop(array){
-  for (i = 0; i < 25; i++) {
-    if(i===1){
-      array.push("I am 1 strange loop.");
-    }else{
-      array.push("I am ${i} strange loops.")
-    }
+  for(var i = 0; i <25; i++){
+    if (i === 1){
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
   }
-  return array;
+}
+return array
 }
 
 function whileLoop(number){
