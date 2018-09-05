844d2429dcbb7b165ba7ce20c0031b23776843b6
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-05 01:36:15 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,32 @@
+var array = [array, t]
+ 
+function forLoop(array){
+  for (i = 0; i < 25; i++) {
+    if(i===1){
+      array.push("I am 1 strange loop.");
+    }else{
+      array.push("I am ${i} strange loop.")
+    }
+  }
+  return array;
+}
+
+function whileLoop(number){
+  while(number>0){
+      console.log(--number);
+    }
+  if(number===0){
+    return ('done');
+    }
+}
+
+function doWhileLoop(array){
+  function incrementVariable() {
+    i = l - 1;
+  }
+  do {
+    console.log();
+    incrementVariable(i);
+  } while (i>0);
+  return array
+}
