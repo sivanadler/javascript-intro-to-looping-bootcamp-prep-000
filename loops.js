a015723ed8121558b55aa24441bf7a906162312e
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-05 01:53:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -26,8 +26,7 @@ function doWhileLoop(array){
   }
   do {
     array.pop
-  } while (array.length === 0)
-  incrementVariable();
+  } while (array.length >0 && incrementVariable());
   return array
 }
 
